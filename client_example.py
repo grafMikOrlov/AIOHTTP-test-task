9d9d14bdfaff1dc82ba12b90ac64b7034178bd49
grafMikOrlov <95017206+grafMikOrlov@users.noreply.github.com>
2022-07-16 14:51:11 +0700

    Create client_example.py

---
+++ client_example.py
@@ -0,0 +1,23 @@
+import aiohttp
+import asyncio
+
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://python.org') as response:
+
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            html = await response.text()
+            print("Body:", html[:15], "...")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
+
+# This prints:
+
+# Status: 200
+# Content-type: text/html; charset=utf-8
+# Body: <!doctype html> ...
