adaa62fdbfee2692ab78b410f4472180f87abb0c
grafMikOrlov <95017206+grafMikOrlov@users.noreply.github.com>
2022-07-16 20:13:46 +0700

    Update client_example.py

--- client_example.py
+++ client_example.py
@@ -1,23 +1,19 @@
-import aiohttp
 import asyncio
+import aiohttp
+
+async def make_request(session, req_n):
+    url = "https://example.com"
+    print(f"making request {req_n} to {url}")
+    async with session.get(url) as resp:
+        if resp.status == 200:
+            await resp.text()
 
 async def main():
-
+    n_requests = 100
     async with aiohttp.ClientSession() as session:
-        async with session.get('http://python.org') as response:
-
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
-
-            html = await response.text()
-            print("Body:", html[:15], "...")
+        await asyncio.gather(
+            *[make_request(session, i) for i in range(n_requests)]
+        )
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
-
-
-# This prints:
-
-# Status: 200
-# Content-type: text/html; charset=utf-8
-# Body: <!doctype html> ...
